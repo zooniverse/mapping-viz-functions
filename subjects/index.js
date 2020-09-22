becb3a93545f15779a1211612fee66318b386ccd
Will Granger <twgranger@gmail.com>
2020-09-22 15:32:40 -0500

    Include error handling

--- subjects/index.js
+++ subjects/index.js
@@ -7,15 +7,32 @@ function executeSQL(context, params) {
     client.connect();
     const { maxLat, minLat, maxLon, minLon } = params
 
+    if (!maxLat || !minLat || !maxLon || !minLon) {
+        context.res = {
+            status: 422,
+            body: 'Query params must include maxLat, minLat, maxLon, and minLon'
+        }
+        return context.done()
+    }
+
     const query = `SELECT * FROM subjects where lat < ${maxLat} and lat > ${minLat}
         and lon < ${maxLon} and lon > ${minLon}`;
 
-    client.query(query).then(res => {
-        context.res = {
-            body: res.rows
-        } 
-        context.done()  
-    })
+    client.query(query)
+        .then(res => {
+            context.res = {
+                status: 200,
+                body: res.rows
+            } 
+            context.done()  
+        })
+        .catch(err => {
+            context.res = {
+                status: 500,
+                body: 'Error connecting to the mapping viz database'
+            }
+            context.done()
+        })
 }
 
 module.exports = function (context, req) {
--- temperature/index.js
+++ temperature/index.js
@@ -6,15 +6,32 @@ function executeSQL(context, params) {
 
     client.connect();
     const { lat, lon } = params
+    
+    if (!lat || !lon) {
+        context.res = {
+            status: 422,
+            body: 'Query params must include both lat and lon'
+        }
+        return context.done()
+    }
 
     const query = `SELECT * FROM temperature where lat = ${lat} and lon = ${lon}`;
 
-    client.query(query).then(res => {
-        context.res = {
-            body: res.rows
-        }
-        context.done()
-    })
+    client.query(query)
+        .then(res => {
+            context.res = {
+                status: 200,
+                body: res.rows
+            }
+            context.done()
+        })
+        .catch(err => {
+            context.res = {
+                status: 500,
+                body: 'Error connecting to the mapping viz database'
+            }
+            context.done()
+        })
 }
 
 module.exports = function (context, req) {
