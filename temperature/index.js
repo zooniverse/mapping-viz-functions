00cea150af327bb28375dc15543c5e8adb137129
Nicole Ciemniak <nciemniak@gmail.com>
2020-09-30 11:38:26 -0500

    update table name to 'temperatures'

--- temperature/index.js
+++ temperature/index.js
@@ -16,7 +16,7 @@ async function executeSQL(context, params) {
     }
     
     client.connect();
-    const query = `SELECT * FROM temperature where temperature_grid_index = $1`;
+    const query = `SELECT * FROM temperatures where temperature_grid_index = $1`;
     const values = [grid_index]
     await performQuery(client, context, query, values)
 }
