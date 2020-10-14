6eb2400f3a4e0f88e9151158ae857b12de1c1531
Nicole Ciemniak <nciemniak@gmail.com>
2020-10-14 17:08:34 -0500

    add schema and test data as sql files

---
+++ db/schema.sql
@@ -0,0 +1,161 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.5.20
+-- Dumped by pg_dump version 12.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+--
+-- Name: subjects; Type: TABLE; Schema: public; Owner: admin_staging
+--
+
+CREATE TABLE public.subjects (
+    id integer NOT NULL,
+    latitude numeric,
+    longitude numeric,
+    date date,
+    metadata jsonb,
+    media_location text,
+    subject_set_id integer,
+    kelp_km2 numeric,
+    temperature_grid_index integer
+);
+
+
+ALTER TABLE public.subjects OWNER TO admin_staging;
+
+--
+-- Name: subjects_id_seq; Type: SEQUENCE; Schema: public; Owner: admin_staging
+--
+
+CREATE SEQUENCE public.subjects_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.subjects_id_seq OWNER TO admin_staging;
+
+--
+-- Name: subjects_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: admin_staging
+--
+
+ALTER SEQUENCE public.subjects_id_seq OWNED BY public.subjects.id;
+
+
+--
+-- Name: temperatures; Type: TABLE; Schema: public; Owner: admin_staging
+--
+
+CREATE TABLE public.temperatures (
+    id integer NOT NULL,
+    latitude numeric,
+    longitude numeric,
+    temp_celsius numeric,
+    date date,
+    temperature_grid_index integer
+);
+
+
+ALTER TABLE public.temperatures OWNER TO admin_staging;
+
+--
+-- Name: temperatures_id_seq; Type: SEQUENCE; Schema: public; Owner: admin_staging
+--
+
+CREATE SEQUENCE public.temperatures_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.temperatures_id_seq OWNER TO admin_staging;
+
+--
+-- Name: temperatures_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: admin_staging
+--
+
+ALTER SEQUENCE public.temperatures_id_seq OWNED BY public.temperatures.id;
+
+
+--
+-- Name: subjects id; Type: DEFAULT; Schema: public; Owner: admin_staging
+--
+
+ALTER TABLE ONLY public.subjects ALTER COLUMN id SET DEFAULT nextval('public.subjects_id_seq'::regclass);
+
+
+--
+-- Name: temperatures id; Type: DEFAULT; Schema: public; Owner: admin_staging
+--
+
+ALTER TABLE ONLY public.temperatures ALTER COLUMN id SET DEFAULT nextval('public.temperatures_id_seq'::regclass);
+
+
+--
+-- Name: subjects subjects_pkey; Type: CONSTRAINT; Schema: public; Owner: admin_staging
+--
+
+ALTER TABLE ONLY public.subjects
+    ADD CONSTRAINT subjects_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: temperatures temperatures_pkey; Type: CONSTRAINT; Schema: public; Owner: admin_staging
+--
+
+ALTER TABLE ONLY public.temperatures
+    ADD CONSTRAINT temperatures_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: azure_superuser
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM azure_superuser;
+GRANT ALL ON SCHEMA public TO azure_superuser;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- Name: TABLE subjects; Type: ACL; Schema: public; Owner: admin_staging
+--
+
+REVOKE ALL ON TABLE public.subjects FROM PUBLIC;
+REVOKE ALL ON TABLE public.subjects FROM admin_staging;
+GRANT ALL ON TABLE public.subjects TO admin_staging;
+GRANT ALL ON TABLE public.subjects TO mapping_viz_staging;
+
+
+--
+-- Name: TABLE temperatures; Type: ACL; Schema: public; Owner: admin_staging
+--
+
+REVOKE ALL ON TABLE public.temperatures FROM PUBLIC;
+REVOKE ALL ON TABLE public.temperatures FROM admin_staging;
+GRANT ALL ON TABLE public.temperatures TO admin_staging;
+GRANT ALL ON TABLE public.temperatures TO mapping_viz_staging;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
